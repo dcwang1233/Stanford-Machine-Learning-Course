66cc1b2385048f4e48c620915694f36664d73eca
dean wang <dcwang1233@gmail.com>
2015-03-21 10:54:17 -0700

    Update kMeansInitCentroids.m

--- K-Means Clustering and PCA/mlclass-ex7/kMeansInitCentroids.m
+++ K-Means Clustering and PCA/mlclass-ex7/kMeansInitCentroids.m
@@ -13,13 +13,8 @@ centroids = zeros(K, size(X, 2));
 %               the dataset X
 %
 
-
-randidx = randperm(size(X , 1));
-centroids = X(randidx(1 : K) ,:);
-
-
-
-
+nrow = rows(X);
+centroids = X(ceil(nrow * rand(K,1)),:)
 
 % =============================================================
 
